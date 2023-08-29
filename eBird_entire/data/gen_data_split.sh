8dad1c1ab0ce202a79b3722c49398b0b3885a73d
Courtney Davis <cld74@cornell.edu>
2023-08-29 08:32:10 -0400

    Update gen_data_split.sh

--- eBird_entire/data/gen_data_split.sh
+++ eBird_entire/data/gen_data_split.sh
@@ -1,7 +1,7 @@
 # For training and model selection
-python3 preprocess_daniel_bird.py
-python3 generate_daniel_idx.py
+python3 preprocess_bird.py
+python3 generate_idx.py
 
 # For testing on the real-world data
-python3 preprocess_daniel_esrd.py
+python3 preprocess_esrd.py
 
