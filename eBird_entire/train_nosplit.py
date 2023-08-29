5bd873113db677b12b94e533bf1653cc5519c101
Courtney Davis <cld74@cornell.edu>
2023-08-29 08:29:08 -0400

    Update train_nosplit.py

--- eBird_entire/train_nosplit.py
+++ eBird_entire/train_nosplit.py
@@ -5,7 +5,7 @@ import os
 for i in range(1):
     for j in range(1, 13):
         job_name = "ebird_%d" % (j)
-        command = "python3 train.py --model_dir ./data/new_model%d/ --summary_dir ./new_summary_%d --visual_dir ./new_vis_%d --data_dir ./data/small_region/daniel_bird_%d.npy --train_idx ./data/small_region/daniel_bird_train_idx_%d.npy --valid_idx ./data/small_region/daniel_bird_val_idx_%d.npy --test_idx ./data/small_region/daniel_bird_test_idx_%d.npy --r_dim 500 --r_max_dim 500 | tee ./data/train_logs/nosplit_ebird_%d" % (j, j, j, j, j, j, j, j)
+        command = "python3 train.py --model_dir ./data/new_model%d/ --summary_dir ./new_summary_%d --visual_dir ./new_vis_%d --data_dir ./data/small_region/bird_%d.npy --train_idx ./data/small_region/bird_train_idx_%d.npy --valid_idx ./data/small_region/bird_val_idx_%d.npy --test_idx ./data/small_region/bird_test_idx_%d.npy --r_dim 500 --r_max_dim 500 | tee ./data/train_logs/nosplit_ebird_%d" % (j, j, j, j, j, j, j, j)
         print (command)
         os.system(command)
 
