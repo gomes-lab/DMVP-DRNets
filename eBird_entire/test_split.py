aa7e6b59efd0d27d6383c82c7de4b7904c42be64
Courtney Davis <cld74@cornell.edu>
2023-08-29 08:28:12 -0400

    Update test_split.py

--- eBird_entire/test_split.py
+++ eBird_entire/test_split.py
@@ -22,7 +22,7 @@ for cnt in range(1, 13):
             cur += 1
             print (cur)
             j_name = "test_bird_%d_%d" % (i, j)
-            command = "python3 test.py --checkpoint_path ./data/small_model_mon%d/model_%d_%d/model-%d --summary_dir ./summary_%d_%d --visual_dir ./vis_%d_%d --data_dir ./data/small_region/small_case%d_daniel_bird_%d.npy --test_idx ./data/small_region/small_case%d_daniel_bird_test_idx_%d.npy --r_dim 500 --r_max_dim 500 --mon %d --case %d | tee ./data/small_map_data%d/%s" % (cnt, i, j, model_num[cur], i, j, i, j, i, j, i, j, j, i, cnt, j_name)
+            command = "python3 test.py --checkpoint_path ./data/small_model_mon%d/model_%d_%d/model-%d --summary_dir ./summary_%d_%d --visual_dir ./vis_%d_%d --data_dir ./data/small_region/small_case%d_bird_%d.npy --test_idx ./data/small_region/small_case%d_bird_test_idx_%d.npy --r_dim 500 --r_max_dim 500 --mon %d --case %d | tee ./data/small_map_data%d/%s" % (cnt, i, j, model_num[cur], i, j, i, j, i, j, i, j, j, i, cnt, j_name)
 
             print (command)
             os.system(command)
